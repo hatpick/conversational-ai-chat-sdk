b026af757c3e44da2ba2349972d5a3bafa06ebf7
William Wong <compulim@users.noreply.github.com>
2024-05-10 08:45:24 +0000

    Add failure test

---
+++ packages/copilot-studio-direct-to-engine-chat-adapter/src/tests/startNewConversation.failure.serverResponse.spec.ts
@@ -0,0 +1,81 @@
+import { HttpResponse, http } from 'msw';
+import { setupServer } from 'msw/node';
+import createHalfDuplexChatAdapter, { type TurnGenerator } from '../createHalfDuplexChatAdapter';
+import type { DefaultHttpResponseResolver } from '../private/types/DefaultHttpResponseResolver';
+import type { HalfDuplexChatAdapterAPIStrategy } from '../private/types/HalfDuplexChatAdapterAPIStrategy';
+import type { JestMockOf } from '../private/types/JestMockOf';
+
+const server = setupServer();
+
+const NOT_MOCKED: DefaultHttpResponseResolver = () => {
+  throw new Error('This function is not mocked.');
+};
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe.each([['rest' as const], ['server sent events' as const]])('Using "%s" transport', transport => {
+  let strategy: HalfDuplexChatAdapterAPIStrategy;
+
+  beforeEach(() => {
+    strategy = {
+      async prepareExecuteTurn() {
+        return Promise.resolve({
+          baseURL: new URL('http://test/?api=execute#2'),
+          body: { dummy: 'dummy' },
+          headers: new Headers({ 'x-dummy': 'dummy' }),
+          transport
+        });
+      },
+      async prepareStartNewConversation() {
+        return Promise.resolve({
+          baseURL: new URL('http://test/?api=start#1'),
+          body: { dummy: 'dummy' },
+          headers: new Headers({ 'x-dummy': 'dummy' }),
+          transport
+        });
+      }
+    };
+  });
+
+  describe.each([true, false])('With emitStartConversationEvent of %s', emitStartConversationEvent => {
+    describe.each([
+      ['server closed connection', HttpResponse.error(), { expectedNumCalled: 5 }],
+      ['server returned 400', new HttpResponse(undefined, { status: 400 }), { expectedNumCalled: 1 }],
+      ['server returned 500', new HttpResponse(undefined, { status: 500 }), { expectedNumCalled: 5 }]
+    ])('when conversation started and %s', (_, response, { expectedNumCalled }) => {
+      let generator: TurnGenerator;
+      let httpPostConversations: JestMockOf<DefaultHttpResponseResolver>;
+
+      beforeEach(async () => {
+        httpPostConversations = jest.fn(NOT_MOCKED);
+
+        server.use(http.post('http://test/conversations', httpPostConversations));
+
+        generator = await createHalfDuplexChatAdapter(strategy, {
+          emitStartConversationEvent,
+          retry: { factor: 1, minTimeout: 0 }
+        })();
+      });
+
+      describe('when iterate', () => {
+        let iteratePromise: Promise<unknown>;
+
+        beforeEach(async () => {
+          httpPostConversations.mockImplementation(() => response);
+
+          iteratePromise = generator.next();
+
+          await iteratePromise.catch(() => {});
+        });
+
+        test(`should have POST to /conversations ${
+          expectedNumCalled === 1 ? 'once' : `${expectedNumCalled} times`
+        }`, () => expect(httpPostConversations).toHaveBeenCalledTimes(expectedNumCalled));
+
+        test('should reject', () => expect(iteratePromise).rejects.toThrow());
+      });
+    });
+  });
+});
