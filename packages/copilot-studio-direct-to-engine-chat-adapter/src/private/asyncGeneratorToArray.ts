066c530d695229e31ecb8c91df0ee9ddd7576a74
William Wong <compulim@users.noreply.github.com>
2024-05-10 10:29:10 +0000

    Update generic types

--- packages/copilot-studio-direct-to-engine-chat-adapter/src/private/asyncGeneratorToArray.ts
+++ packages/copilot-studio-direct-to-engine-chat-adapter/src/private/asyncGeneratorToArray.ts
@@ -1,6 +1,9 @@
 import iterateWithReturnValue from './iterateWithReturnValue';
 
-export default async function asyncGeneratorToArray<T, U>(asyncGenerator: AsyncGenerator<T, U>): Promise<[T[], U]> {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default async function asyncGeneratorToArray<T, TReturn = any>(
+  asyncGenerator: AsyncGenerator<T, TReturn>
+): Promise<[T[], TReturn]> {
   const array: T[] = [];
   const [iterator, getReturnValue] = iterateWithReturnValue(asyncGenerator);
 
